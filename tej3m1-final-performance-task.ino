d83bd37ab13cfc3b7b4d106b3c2e6f1a6b1b90dd
Jason <jasonli.0616@outlook.com>
2023-06-05 13:21:09 -0400

    Start creation of project
    - Add servo and photoresistors
    
    - Moving servo based on photoresistors

--- tej3m1-final-performance-task.ino
+++ tej3m1-final-performance-task.ino
@@ -1,11 +1,53 @@
 #include <Servo.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define SERVO_PIN 10
+#define PHOTORESISTOR_PIN_1 A0
+#define PHOTORESISTOR_PIN_2 A1
+#define PHOTORESISTOR_PIN_3 A2
 
+
+// Objects
+Servo servo;
+
+// State
+int photoresistorValue1;
+int photoresistorValue2;
+int photoresistorValue3;
+int servoPosition = 90;
+
+
+void setup() {
+  // Serial monitor
+  Serial.begin(9600);
+  
+  // Set pins
+  servo.attach(10);
+  pinMode(PHOTORESISTOR_PIN_1, OUTPUT);
+  pinMode(PHOTORESISTOR_PIN_2, OUTPUT);
+  pinMode(PHOTORESISTOR_PIN_3, OUTPUT);
+
+  // Default state
+  servo.write(servoPosition);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+
+  // Get photoresistor values
+  photoresistorValue1 = analogRead(PHOTORESISTOR_PIN_1);
+  photoresistorValue2 = analogRead(PHOTORESISTOR_PIN_2);
+  photoresistorValue3 = analogRead(PHOTORESISTOR_PIN_3);
+  
+}
+
+/* Move the photoresistor to the bright position, indicated by the photoresistors. */
+void moveServoBasedOnPhotoresistor() {
+
+  // Temporary solution. Change method later.
+  if (photoresistorValue1 > photoresistorValue2 && photoresistorValue1 > photoresistorValue3)
+    servo.write(0);
+  else if (photoresistorValue2 > photoresistorValue1 && photoresistorValue2 > photoresistorValue3)
+    servo.write(90);
+  else if (photoresistorValue3 > photoresistorValue2 && photoresistorValue3 > photoresistorValue1)
+    servo.write(180);
 }
